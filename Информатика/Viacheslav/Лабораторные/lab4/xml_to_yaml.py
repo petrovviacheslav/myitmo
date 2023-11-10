b0d8daa5c61e0987e6cda5da626301ac2a85e499
Viacheslav <test@test.com>
2023-11-11 01:39:22 +0300

    :sparkles: add new files

--- Информатика/Viacheslav/Лабораторные/lab4/xml_to_yaml.py
+++ Информатика/Viacheslav/Лабораторные/lab4/xml_to_yaml.py
@@ -1,21 +1,55 @@
+arr = []
+arr_tag = [[], [], [], [], []]
+counter_tag = [0, 0, 0, 0]
 
-def create_str_yaml(line):
-    gaps, tag, text, = separation(line)
+prev_tag = ''
 
-    if '/' in tag:
-        print('')
-    else:
-        print(gaps * ' ' + tag + ': ' + text)
+
+def create_str_yaml(arr):
+    for elem in arr:
+        find_tags(elem)
+    with open("finish.yaml", "w", encoding="utf-8") as out:
+        minus_flag = 0
+        for i in range(len(arr)):
+            gaps, tag, text, = separation(arr[i])
+
+            if '/' in tag:
+                pass
+            else:
+                ind = counter_tag[gaps // 4]
+                if ind > 0 and arr_tag[gaps // 4][ind] == arr_tag[gaps // 4][ind - 1]:
+                    print(gaps * ' ' + '  - ', file=out, end='')
+                    if len(text) > 0:
+                        print(text, file=out)
+                    minus_flag = 1
+                elif len(arr_tag[gaps // 4]) > (ind + 1) and arr_tag[gaps // 4][ind] == arr_tag[gaps // 4][ind + 1]:
+                    print(gaps * ' ' + tag + ': ', file=out)
+                    print(gaps * ' ' + '  - ', file=out, end='')
+                    if len(text) > 0:
+                        print(text, file=out)
+                    minus_flag = 1
+                else:
+                    if minus_flag:
+                        print(tag + ': ' + text, file=out)
+                    else:
+                        print(gaps * ' ' + tag + ': ' + text, file=out)
+                    minus_flag = 0
+
+                counter_tag[gaps // 4] += 1
 
 
 def separation(line):
     gaps = line.find('<')
-
     tag = line[line.find('<') + 1: line.find('>')]
-
     text = line[line.find('>') + 1: line.find('</')]
-
     return gaps, tag, text
+
+
+def find_tags(line):
+    if '/' in line[line.find('<') + 1: line.find('>')]:
+        pass
+    else:
+        arr_tag[line.find('<') // 4].append(line[line.find('<') + 1: line.find('>')])
 
 
 with open("start.xml", "r", encoding="utf-8") as f:
@@ -23,4 +57,5 @@ with open("start.xml", "r", encoding="utf-8") as f:
         if elem == '<?xml version="1.0" encoding="UTF-8" ?>\n':
             continue
         else:
-            create_str_yaml(elem)
+            arr.append(elem)
+    create_str_yaml(arr)
---
+++ Информатика/Viacheslav/Лабораторные/lab5/doptask.py
@@ -0,0 +1,39 @@
+import csv
+import pandas as pd
+import seaborn as sns
+import matplotlib.pyplot as plt
+
+
+def convert(arg):
+    return int(arg[:arg.find('.')])
+
+
+with open("start.csv", encoding='utf-8') as r_file:
+    file_reader = csv.reader(r_file, delimiter=",")
+    arr = [[],[],[],[]]
+    col = ["Открытие", "Мин", "Макс", "Закрытие"]
+    dates = ['05/09/18', '05/10/18', '07/11/18', '05/12/18']
+    count = 0
+    for row in file_reader:
+        if count == 0:
+            pass
+            # col = [row[4], row[5], row[6], row[7]]
+        elif row[2] == '05/09/18':
+            arr[0].append([convert(row[4]), convert(row[5]), convert(row[6]), convert(row[7])])
+        elif row[2] == '05/10/18':
+            arr[1].append([convert(row[4]), convert(row[5]), convert(row[6]), convert(row[7])])
+        elif row[2] == '07/11/18':
+            arr[2].append([convert(row[4]), convert(row[5]), convert(row[6]), convert(row[7])])
+        elif row[2] == '05/12/18':
+            arr[3].append([convert(row[4]), convert(row[5]), convert(row[6]), convert(row[7])])
+
+        count += 1
+    plt.figure(figsize=(12, 6))
+    for i in range(4):
+        data = pd.DataFrame(arr[i], columns=col)
+        plt.subplot(2, 2, i+1)
+        sns.boxplot(data=data, palette='husl')
+        plt.title(f"Данные на {dates[i]}")
+        plt.legend(col, loc='best')
+    plt.subplots_adjust(wspace=0.4, hspace=0.4)
+    plt.show()
