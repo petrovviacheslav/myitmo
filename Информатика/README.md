77ec56d36caed1382433c8859d6bae382c14b7f6
Viacheslav <test@test.com>
2023-11-19 01:07:08 +0300

    :hammer: a lot of changes

--- Информатика/README.md
+++ Информатика/README.md
@@ -26,5 +26,7 @@
 - https://github.com/maxbarsukov/itmo/tree/master/1%20%D0%B8%D0%BD%D1%84%D0%BE%D1%80%D0%BC%D0%B0%D1%82%D0%B8%D0%BA%D0%B0/%D0%BB%D0%B0%D0%B1%D0%BE%D1%80%D0%B0%D1%82%D0%BE%D1%80%D0%BD%D1%8B%D0%B5/lab4
 - https://github.com/belovlaska/itmo/tree/main/Informatics/lab4
 
-
 ## 5 лаба
+
+
+<a href="">ghbhdhdhdhd</a>
--- Программирование/Viacheslav/lab2/script.sh
+++
@@ -1,4 +0,0 @@
-export CLASSPATH=./lib/Pokemon.jar:./src:./src/info/Moves/PhysicalMoves:./src/info/Moves/StatusMoves./src/info/Moves/SpecialMoves./src/info/Pokemons:./src/info/Main
-javac -d out src/info/Main.java
-echo -e "Manifest-Version: 1.0\nClass-Path: lib/Pokemon.jar\nMain-Class: info.Main\n" > MANIFEST.MF
-jar cfm Main.jar MANIFEST.MF -C out . -C . lib/ru
